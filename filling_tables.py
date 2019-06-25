32848afe77392ef059b21836bf05c84d4db9f4e3
Alexander Shukelovich <shuk.san4@yandex.ru>
2019-06-25 03:38:10 +0300

    Added choice columns for filling

--- filling_tables.py
+++ filling_tables.py
@@ -32,18 +32,40 @@ def log(message):
 	with open(os.path.join(script_dir, 'log.txt'), 'a') as output:
 		output.write(message)
 
-def filling_table(check, parse_table, data):
-	columns = info_take(f"""SELECT column_name, data_type FROM information_schema.columns
-		WHERE information_schema.columns.table_name = '{parse_table}'""")
-	columns_names = [x[0] for x in columns]
-	columns_types = [x[1] for x in columns]
+def filling_table(check, parse_table, parse_columns, data):
+	columns_db = info_take(f"""SELECT column_name, data_type, column_default FROM information_schema.columns
+	WHERE information_schema.columns.table_name = '{parse_table}'""")
+	columns_db_names = [x[0] for x in columns_db]
+	# columns_db_types = [x[1] for x in columns_db]
+	columns_db_default = [x[2] for x in columns_db]
+	
+	
+	col = 0
+	def_val = 0
+	parse_columns = parse_columns.split(', ')
+	while col < len(parse_columns):
+		while def_val < len(columns_db_default):	
+			if columns_db_default[def_val] is not None:
+				parse_columns.insert(def_val, columns_db_names[def_val])
+				data = data.split(', ')
+				data.insert(def_val, 'DEFAULT')
+				data = ', '.join(data)
+			def_val += 1
+		col += 1
+	parse_columns = ', '.join(parse_columns)
+	
+	
 	data = data.split(', ')
-	request = f"""INSERT INTO {parse_table} VALUES ()"""
+	request = f"""INSERT INTO "{parse_table}" () VALUES ()"""
+	request = request[:request.find('VALUES')-2] + parse_columns + request[request.find('VALUES')-2:]
 	for value in data[:-1]:
-		if not value.isdigit():
+		if not value.isdigit() and value != 'DEFAULT':
 			value = "'" + value + "'"
 		request = request[:-1] + value + ', ' + request[-1:]
-	request = request[:-1] + data[-1] + request[-1:]
+	if not data[-1].isdigit() and data[-1] != 'DEFAULT':
+		request = request[:-1] + "'" + data[-1] + "'" + request[-1:]
+	else:
+		request = request[:-1] + data[-1] + request[-1:]
 	print(request)
 	# info_give(request)
 
@@ -54,7 +76,8 @@ with open(os.path.join(script_dir, 'parse_data.txt'), 'r', encoding='utf-8') as 
 	data.close()
 
 def parsing(lines):
-	parse_tables = []
+	tables = []
+	columns = []
 	data = []
 	tables_db = info_take("""SELECT table_name FROM information_schema.tables
 	WHERE table_schema = 'public'""")
@@ -68,25 +91,39 @@ def parsing(lines):
 				log(f'Таблица "{parse_table}" не найдена.\n')
 				print(f'Таблица "{parse_table}" не найдена.')
 				break
-			parse_tables.append(parse_table)
+			tables.append(parse_table)
 
 			parse_columns = row[row.find('[')+1:row.find(']')].split(',')
 			for i, column in enumerate(parse_columns):
 				parse_columns[i] = column.strip()
 			columns_db = info_take(f"""SELECT column_name, data_type, column_default FROM information_schema.columns
 			WHERE information_schema.columns.table_name = '{parse_table}'""")
-			columns_db = [x[0] for x in columns_db]
+			columns_db_names = [x[0] for x in columns_db]
 			for column in parse_columns:
-				if column not in columns_db:
+				if column not in columns_db_names:
 					log(f'Столбец "{column}" не найден.\n')
 					print(f'Столбец "{column}" не найден.')
+					break
+
+			parse_columns = ', '.join(parse_columns)
+			columns.append(parse_columns)
 		else:
 			data.append(row.strip())
-	return parse_tables, data
+	return tables, columns, data
 
-parse_tables, data = parsing(lines)
+parse_tables, parse_columns, data = parsing(lines)
 print(parse_tables)
+print(parse_columns)
 print(data)
-for row in data:
-	pass
-# filling_table(0, parse_table, data)
+
+table = 0
+row = 0
+while table < len(parse_tables):
+	while row < len(data):
+		if data[row] != 'delimeter for new table':
+			filling_table(0, parse_tables[table], parse_columns[table], data[row])
+			row += 1
+		else:
+			row += 1
+			break
+	table += 1
